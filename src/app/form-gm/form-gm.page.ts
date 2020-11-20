1d28f3d9cafc4d5767659ddabf35fb1c6c5eb9b4
Desarrollo <bereflores2706@gmail.com>
2020-11-20 10:18:16 -0800

    Modificación de vistas

--- src/app/form-gm/form-gm.page.ts
+++ src/app/form-gm/form-gm.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form-gm',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormGmPage implements OnInit {
 
-  constructor() { }
-
+  @ViewChild('direccion', { read: ElementRef }) direccion: ElementRef;
+  @ViewChild('rescatejt', { read: ElementRef }) rescatejt: ElementRef;
+  
+  constructor(private renderer: Renderer2) { }
+  
   ngOnInit() {
+    
+  }
+  ngAfterViewInit() {
+    console.log(this.rescatejt.nativeElement.value);
+    if(this.rescatejt.nativeElement.value==="Rescate"){
+      this.renderer.setStyle(this.direccion.nativeElement, 'display', '');
+    }
+  }
+  
+  show(event){ 
+    console.log(this.rescatejt.nativeElement.value);
+    if(event.target.value==="Rescate")
+      this.renderer.setStyle(this.direccion.nativeElement, 'display', '');
+    else
+      this.renderer.setStyle(this.direccion.nativeElement, 'display', 'none');
   }
 
 }
--- src/app/form-jt/form-jt.page.ts
+++ src/app/form-jt/form-jt.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-form-jt',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormJTPage implements OnInit {
 
-  constructor() { }
+  @ViewChild('direccion', { read: ElementRef }) direccion: ElementRef;
+  
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+  
+  show(event){ 
+    console.log(event.target.value);
+    if(event.target.value==="Rescate")
+      this.renderer.setStyle(this.direccion.nativeElement, 'display', '');
+    else
+      this.renderer.setStyle(this.direccion.nativeElement, 'display', 'none');
   }
 
 }
